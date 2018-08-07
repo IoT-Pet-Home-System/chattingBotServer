2b0651502e8b2bc79ce5de0da3c1bdb5377fe4c7
woojin <33398268+kuj0210@users.noreply.github.com>
2018-08-07 13:28:16 +0900

    Update IDissuance.py

--- src/auth/IDissuance.py
+++ src/auth/IDissuance.py
@@ -9,19 +9,23 @@ import time
 
 class IDIssuance:
     def getTempID(self, userKey):
+        
         if len(userKey) <= 6 or userKey == None:
             raise Exception('Too Short String, Please enter at least 6 digits of text.')
-
+            
         Tid = ""
         Charlist = list(userKey)
         alph = 0
         limit = len(Charlist)
+        
         for cnt in range(limit):
             item = Charlist.pop(0)
             alph += ord(item)
+            
             if (cnt != 0 and cnt % 2 == 0) or range(len(Charlist)) == 0:
                 Tid += chr(alph % 26 + 65)
                 alph = 0
+                
         sec = time.localtime().tm_sec
         return Tid + str(sec)
 
@@ -32,7 +36,7 @@ if __name__ == "__main__":
     print("input = " + UK)
     tid = TID.getTempID(UK)
     print("output = " + tid)
-
+    # ERR_Check
     UK = "a231"
     print("input = " + UK)
     tid = TID.getTempID(UK)
